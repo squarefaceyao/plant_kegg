4d65a8cf2c9bef857912135ec80ecf6a8dec8eed
squarefaceyao <squarefaceyao@gmail.com>
2021-07-15 21:48:04 +0800

    ath all gene information

---
+++ DES2Gene.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''=================================================
+@Project -> File   ：plant-kegg -> DES2Gene
+@IDE    ：PyCharm
+@Author ：Mr. Y
+@Date   ：2021-07-13 10:54
+@Desc   ：
+=================================================='''
